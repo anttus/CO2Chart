66bc1f9dbbe30fe8b0ff396430ce060deba25039
anttus <anttu.suhonen@gmail.com>
2019-01-07 14:29:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8082;
+// const routes = require('./routes/routes');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set('view engine', 'jade');
+
+app.listen(port, () => console.log('API listening on port ' + port))
+
+// routes(app);
+
